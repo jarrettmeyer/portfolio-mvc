4131fbb3ee4bf7bab2a8afe4234fbb51ef4df374
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-09-19 18:18:20 -0400

    Working on grid for knockout

--- src/Portfolio.Web/Scripts/App/Home.Index.js
+++ src/Portfolio.Web/Scripts/App/Home.Index.js
@@ -1,3 +1,34 @@
-﻿(function() {
+﻿(function () {
 
+    var TasksViewModel = function (tasks, pageSize) {
+        var self = this;
+
+        self.tasks = ko.observableArray(tasks);
+        self.pageSize = ko.observable(pageSize);
+
+        self.grid = new ko.grid.viewModel({
+            data: self.tasks,
+            columns: [
+                { headerText: "Description", rowText: "Description" },
+                { headerText: "Created On", rowText: "CreatedAt" },
+                { headerText: "Due On", rowText: "DueOn" }
+            ],
+            pageSize: self.pageSize
+        });
+    };
+
+    var initialData = [
+        { Id: 1, Description: "Test 1", CreatedAt: new Date(), DueOn: new Date(2013, 11, 31) },
+        { Id: 2, Description: "Test 2", CreatedAt: new Date(), DueOn: new Date(2013, 11, 31) },
+        { Id: 3, Description: "Test 3", CreatedAt: new Date(), DueOn: new Date(2013, 11, 31) },
+        { Id: 4, Description: "Test 4", CreatedAt: new Date(), DueOn: new Date(2013, 11, 31) },
+        { Id: 5, Description: "Test 5", CreatedAt: new Date(), DueOn: new Date(2013, 11, 31) },
+        { Id: 6, Description: "Test 6", CreatedAt: new Date(), DueOn: new Date(2013, 11, 31) },
+        { Id: 7, Description: "Test 7", CreatedAt: new Date(), DueOn: new Date(2013, 11, 31) },
+        { Id: 8, Description: "Test 8", CreatedAt: new Date(), DueOn: new Date(2013, 11, 31) },
+        { Id: 9, Description: "Test 9", CreatedAt: new Date(), DueOn: new Date(2013, 11, 31) },        
+    ];
+
+    window.tasksViewModel = new TasksViewModel(initialData, 50);
 })();
+
---
+++ src/Portfolio.Web/Scripts/App/ko.grid.js
@@ -0,0 +1,65 @@
+﻿(function () {
+    var defaultTableName = "ko_grid_table",
+        defaultPagingName = "ko_grid_paging";
+
+    ko.grid = {
+        viewModel: function (configuration) {
+            var self = this;
+
+            self.data = configuration.data;
+            self.columns = configuration.columns;
+            self.pageSize = configuration.pageSize || 5;
+
+            self.currentPageIndex = ko.observable(0);
+
+            self.itemsOnCurrentPage = ko.computed(function () {
+                var startIndex = self.pageSize * self.currentPageIndex();
+                return self.data.slice(startIndex, startIndex + self.pageSize);
+
+            }, self);
+
+            self.maxPageIndex = ko.computed(function () {
+                return Math.ceil(ko.utils.unwrapObservable(self.data).length / self.pageSize) - 1;
+            }, self);
+        }
+    };
+
+    var templateEngine = new ko.nativeTemplateEngine();
+
+    templateEngine.addTemplate = function(templateName, templateMarkup) {
+        document.write("<script type=\"text/html\" id=\"" + templateName + "\">" + templateMarkup + "</script>");
+    };
+
+    templateEngine.addTemplate(defaultTableName, "");
+
+    templateEngine.addTemplate(defaultPagingName, "");
+    
+    ko.bindingHandlers.grid = {
+        init: function () {
+            return { 'controlsDescendantBindings': true };
+        },
+        // This method is called to initialize the node, and will also be called again if you change what the grid is bound to
+        update: function (element, viewModelAccessor, allBindingsAccessor) {
+            var viewModel = viewModelAccessor(), allBindings = allBindingsAccessor();
+
+            // Empty the element
+            while (element.firstChild) {
+                ko.removeNode(element.firstChild);
+            }
+                
+
+            // Allow the default templates to be overridden
+            var gridTableTemplate = allBindings.gridTableTemplate || defaultTableName,
+                gridPagingTemplate = allBindings.gridPagingTemplate || defaultPagingName;
+
+            // Render the main grid
+            var tableContainer = element.appendChild(document.createElement("DIV"));
+            ko.renderTemplate(gridTableTemplate, viewModel, { templateEngine: templateEngine }, tableContainer, "replaceNode");
+
+            // Render the page links
+            var pagingContainer = element.appendChild(document.createElement("DIV"));
+            ko.renderTemplate(gridPagingTemplate, viewModel, { templateEngine: templateEngine }, pagingContainer, "replaceNode");
+        }
+    };
+
+})();
