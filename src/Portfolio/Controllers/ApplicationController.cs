53fabf0302473daf0b2e025e3fdb76151b207d2f
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-11-11 11:06:34 -0500

    added invalid model state dictionary exception

--- src/Portfolio/Controllers/ApplicationController.cs
+++ src/Portfolio/Controllers/ApplicationController.cs
@@ -1,8 +1,14 @@
 ﻿using System.Web.Mvc;
+using Portfolio.Lib;
 
 namespace Portfolio.Controllers
 {
     public abstract class ApplicationController : Controller
-    {        
+    {
+        public virtual void CheckModelState()
+        {
+            if (!ViewData.ModelState.IsValid)
+                throw new InvalidModelStateException("", ViewData.ModelState);
+        }
     }
 }
---
+++ src/Portfolio/Lib/InvalidModelStateException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Portfolio.Lib
+{
+    public class InvalidModelStateException : Exception
+    {
+        private readonly string message;
+        private readonly ModelStateDictionary modelState;
+
+        public InvalidModelStateException(string message, ModelStateDictionary modelState)
+        {
+            this.message = message;
+            this.modelState = modelState;
+        }
+
+        public override string Message
+        {
+            get { return message; }
+        }
+
+        public virtual ModelStateDictionary ModelState
+        {
+            get { return modelState; }
+        }
+    }
+}
