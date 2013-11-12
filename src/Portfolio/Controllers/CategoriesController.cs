03353c9a126c8b05425c7b903430e48ec2ba1d30
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-11-12 07:36:52 -0500

    can save new category

--- src/Portfolio/Controllers/CategoriesController.cs
+++ src/Portfolio/Controllers/CategoriesController.cs
@@ -1,25 +1,39 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using Portfolio.Lib.Data;
 using Portfolio.Models;
+using Portfolio.ViewModels;
 
 namespace Portfolio.Controllers
 {
     public class CategoriesController : Controller
     {
-        private readonly IRepository repository;
-
-        public CategoriesController()
-        {
-            repository = Repository.Instance;
-        }
-
-        [HttpGet]
+       [HttpGet]
         public ActionResult Index()
         {
-            var categories = repository.FindAll<Category>().OrderBy(c => c.Description).ToArray();
+            var categories = Repository.Instance.FindAll<Category>().OrderBy(c => c.Description).ToArray();
             return View("Index", categories);
         }
 
+        [HttpGet]
+        public ActionResult New()
+        {
+            var model = new CategoryInputModel();
+            return View("New", model);
+        }
+
+        [HttpPost]
+        public ActionResult New(CategoryInputModel model)
+        {
+            var category = new Category
+            {
+                Description = model.Description.Trim(),
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+            Repository.Instance.Add(category);
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ src/Portfolio/ViewModels/CategoryInputModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Portfolio.ViewModels
+{
+    public class CategoryInputModel
+    {
+        public string ActionName
+        {
+            get { return IsNew ? "New" : "Edit"; }
+        }
+
+        public string ControllerName
+        {
+            get { return "Categories"; }
+        }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(200, ErrorMessage = "Description cannot exceed 200 characters")]
+        public string Description { get; set; }
+
+        public object FormCss
+        {
+            get { return new { @class = "form-horizontal" }; }
+        }
+
+        public FormMethod FormMethod
+        {
+            get { return FormMethod.Post; }
+        }
+
+        public int Id { get; set; }
+
+        public bool IsNew
+        {
+            get { return Id == 0; }
+        }
+
+        public string PageTitle
+        {
+            get { return IsNew ? "New Category" : "Edit Category"; }
+        }
+    }
+}
