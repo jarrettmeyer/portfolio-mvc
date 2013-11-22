eab54fa0ccc9ba8351ba7970e4aebe0830ebe7f6
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-11-22 14:33:27 -0500

    Updates to database project.

--- src/PortfolioDB/Tables/Categories.sql
+++ src/PortfolioDB/Tables/Categories.sql
@@ -1,9 +1,14 @@
 ﻿CREATE TABLE [dbo].[Categories]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+	[Id] INT NOT NULL IDENTITY(1, 1), 
     [Description] VARCHAR(256) NOT NULL,
 	[IsActive] BIT NOT NULL DEFAULT((1)), 
     [CreatedAt] DATETIME NOT NULL, 
     [UpdatedAt] DATETIME NOT NULL, 
     [Version] ROWVERSION NOT NULL
+	CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
+GO
+
+CREATE UNIQUE INDEX [IX_Categories_Description] ON [dbo].[Categories] ([Description])
+GO
---
+++ src/PortfolioDB/Tables/Tasks_Categories.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Tasks_Categories]
+(
+	[TaskId] INT NOT NULL,
+    [CategoryId] INT NOT NULL, 
+    CONSTRAINT [PK_Tasks_Categories] PRIMARY KEY CLUSTERED ([TaskId], [CategoryId])
+)
+GO
