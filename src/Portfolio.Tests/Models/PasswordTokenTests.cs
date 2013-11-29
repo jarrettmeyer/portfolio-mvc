5946bc0387debc14dde6051ffc31dcab931f3cae
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-11-29 09:53:17 -0500

    Added unit tests for PasswordToken

---
+++ src/Portfolio.Tests/Models/PasswordTokenTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Portfolio.Models
+{
+    [TestFixture]
+    public class PasswordTokenTests
+    {
+        private PasswordToken passwordToken;
+
+        [Test]
+        public void Can_generate_a_new_token()
+        {
+            passwordToken = PasswordToken.GenerateForUser(new User());
+            passwordToken.Token.Length.Should().Be(32);
+        }
+    }
+}
