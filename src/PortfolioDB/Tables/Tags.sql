87cdef9bd8552fd148f5d42694ebe920cb543f67
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-11-30 06:14:48 -0500

    Restores tag_id column in tags table.

--- src/PortfolioDB/Tables/Tags.sql
+++ src/PortfolioDB/Tables/Tags.sql
@@ -1,14 +1,18 @@
 ﻿CREATE TABLE [dbo].[tags]
 (
-	[tag_id] VARCHAR(256) NOT NULL,
+	[tag_id] INT NOT NULL IDENTITY(1, 1),
+	[slug] VARCHAR(256) NOT NULL,
     [description] VARCHAR(256) NOT NULL,	
 	[is_active] BIT NOT NULL DEFAULT((1)), 
-    [created_at] DATETIME NOT NULL, 
-    [updated_at] DATETIME NOT NULL, 
+    [created_at] DATETIME NOT NULL DEFAULT GETUTCDATE(),
+    [updated_at] DATETIME NOT NULL DEFAULT GETUTCDATE(), 
     [version] ROWVERSION NOT NULL
 	CONSTRAINT [pk_tags] PRIMARY KEY CLUSTERED ([tag_id] ASC)
 )
 GO
 
-CREATE UNIQUE INDEX [tags_description] ON [dbo].[tags] ([description])
+CREATE UNIQUE INDEX [unique_slug] on [dbo].[tags] ([slug])
+GO
+
+CREATE UNIQUE INDEX [unique_description] ON [dbo].[tags] ([description])
 GO
--- src/PortfolioDB/Tables/Tasks_Tags.sql
+++ src/PortfolioDB/Tables/Tasks_Tags.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tasks_tags]
 (
 	[task_id] INT NOT NULL,
-    [tag_id] VARCHAR(256) NOT NULL, 
+    [tag_id] INT NOT NULL, 
     CONSTRAINT [pk_tasks_tags] PRIMARY KEY CLUSTERED ([task_id], [tag_id]), 
     CONSTRAINT [task_id] FOREIGN KEY ([task_id]) REFERENCES [tasks]([task_id]) ON DELETE CASCADE,
 	CONSTRAINT [tag_id] FOREIGN KEY ([tag_id]) REFERENCES [tags]([tag_id]) ON DELETE CASCADE ON UPDATE CASCADE
